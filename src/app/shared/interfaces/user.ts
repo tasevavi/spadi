066711c52fb1d7364b27d5e033eeb96376ede526
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2022-10-05 15:30:33 +0300

    Add type null to user interface

--- src/app/shared/interfaces/user.ts
+++ src/app/shared/interfaces/user.ts
@@ -1,10 +1,10 @@
 import { Post } from "./post";
 
 export interface User {
-    _id: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    locationCity: string;
+    _id: string | null;
+    email: string | null;
+    firstName: string | null;
+    lastName: string | null;
+    locationCity: string | null;
     posts: Post[];
 }
