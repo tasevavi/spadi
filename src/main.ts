b040151bf76f224e32e79a370a27084da60240b1
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2022-11-01 15:14:28 +0200

    Initialize Firebase in main.ts and export DB

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
+const app = initializeApp(environment.firebaseConfig);
+export const db = getFirestore(app);
+
 if (environment.production) {
   enableProdMode();
 }
