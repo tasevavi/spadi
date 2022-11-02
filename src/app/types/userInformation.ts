f791f988c13f3e89c83c05d4b466ce13f5125b65
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2022-11-02 16:27:00 +0200

    Add type UserInformation

---
+++ src/app/types/userInformation.ts
@@ -0,0 +1,17 @@
+export class UserInformation {
+    email: string
+    firstName: string
+    lastName: string
+    nickName: string
+    locationCity: string
+
+    constructor() {
+        this.email = '',
+        this.firstName = '',
+        this.lastName = '',
+        this.nickName = '',
+        this.locationCity = ''
+    }
+}
+
+//TODO: add an array of postings and donations made
