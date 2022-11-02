2088c943167dd54a85bcdc8cd36fb52b1fcac559
Violeta Taseva <87774260+tasevavi@users.noreply.github.com>
2022-11-02 16:42:54 +0200

    Set user information in profile page

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from 'firebase/auth';
+import { UserInformation } from 'src/app/types/userInformation';
 import { UserService } from '../user.service';
 
 @Component({
@@ -10,31 +11,28 @@ import { UserService } from '../user.service';
 })
 export class ProfileComponent implements OnInit {
   
+  currentUser: User | undefined;
   profilePictureSrc: string = "https://avatars.githubusercontent.com/u/87774260?v=4"
   showEditForm: boolean = false;
-  user: User | undefined;
+  user: any = new UserInformation()
   userUID: string | undefined;
-  userFirstName: string = '';
-  userLastName: string = '';
-  userNames: string = '';
-  userNickName: string = '';
-  userCity: string = '';
-  userPostings: string = '4'
+
   constructor(private userService: UserService) { }
   
   ngOnInit(): void {
-    this.user = this.userService.user;
-    this.userUID = this.user?.uid;
+    this.currentUser = this.userService.user;
+    this.userUID = this.currentUser?.uid;
     this.setCurrentUserInformation();
   }
 
-  async setCurrentUserInformation() {
-    const currentUserPromise = await this.userService.findUserByUid(this.userUID);
-    this.userFirstName = currentUserPromise != undefined ? currentUserPromise['firstName'] : '';
-    this.userLastName = currentUserPromise != undefined ? currentUserPromise['lastName'] : '';
-    this.userNames = this.userFirstName + ' ' + this.userLastName;
-    this.userNickName = currentUserPromise != undefined ? currentUserPromise['nickName'] : '';
-    this.userCity = currentUserPromise != undefined ? currentUserPromise['locationCity'] : '';
+  setCurrentUserInformation() {
+    this.userService.findUserByUid(this.userUID).then(data => {
+      if (data !== undefined) {
+        Object.keys(data).forEach(key => {
+          this.user[key] = data[key];
+        });
+      }
+    });
   }
 
   changeUserInformation(form: NgForm) {
@@ -44,7 +42,6 @@ export class ProfileComponent implements OnInit {
       console.log('invalid form');
       return; 
     }
-    //this.userService.setUserName(firstName, lastName, city, country, this.userUID);
     this.showOrHideEditForm();
   }
 
